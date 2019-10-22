4c71f71a6fc2a9db22ea76b318290eb93297ca97
yaruliu <yarusophia@gmail.com>
2019-10-22 10:23:04 -0400

    add axis

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -8,7 +8,6 @@ class D3Chart {
 	constructor(element, data) {
 		let vis = this
 		vis.data = data
-
 		vis.g = d3.select(element)
 			.append("svg")
 				.attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
@@ -21,7 +20,26 @@ class D3Chart {
 			.range([0 , WIDTH])
 		vis.y = d3.scaleLinear()
 			.range([HEIGHT , 0])
-		console.log(vis.y(0))
+		
+		// axis
+		vis.xAxisGroup = vis.g.append("g")
+			.attr("transform", `translate(0, ${HEIGHT})`)
+		vis.yAxisGroup = vis.g.append("g")
+
+		// text
+		vis.g.append("text")
+			.attr("x", WIDTH / 2)
+			.attr("y", HEIGHT + 40)
+			.attr("font-size", 15)
+			.attr("text-anchor", "middle")
+			.text("Age")
+		vis.g.append("text")
+			.attr("x", -(HEIGHT / 2))
+			.attr("y", -50)
+			.attr("transform", "rotate(-90)")
+			.attr("font-size", 15)
+			.attr("text-anchor", "middle")
+			.text("Height in cm")
 
 		vis.update()		
 	}
@@ -30,7 +48,31 @@ class D3Chart {
 		let vis = this
 		vis.x.domain([0, d3.max(vis.data, d => Number(d.age))])
 		vis.y.domain([0, d3.max(vis.data, d => Number(d.height))])
-	
+
+		const xAxisCall = d3.axisBottom(vis.x)
+		const yAxisCall = d3.axisLeft(vis.y)
+
+		vis.xAxisGroup.call(xAxisCall)
+		vis.yAxisGroup.call(yAxisCall)
+		
+		// JOIN
+		const circles = vis.g.selectAll("circle")
+			.data(vis.data, d => d.name)
+
+		// EXIT
+		circles.exit().remove()
+
+		// UPDATE
+		circles
+			.attr("cx", d => vis.x(d.age))
+			.attr("cy", d => vis.y(d.height))
+
+		// ENTER
+		circles.enter().append("circle")
+			.attr("cx", d => vis.x(d.age))
+			.attr("cy", d => vis.y(d.height))
+			.attr("r", 5)
+			.attr("fill", "grey")
 	}
 }
 
